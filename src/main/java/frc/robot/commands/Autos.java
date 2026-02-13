2b6effaaf39e77095a80034a864ecfb56e650a46
Riley2549 <rileyhh2901@gmail.com>
2026-02-13 10:52:48 -0600

    Auto working

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -12,21 +12,11 @@ import frc.robot.subsystems.CANFuelSubsystem;
 import frc.robot.subsystems.CANDriveSubsystem;
 
 public final class Autos {
-  // Example autonomous command which drives forward for 1 second.
   public static final Command exampleAuto(CANDriveSubsystem driveSubsystem, CANFuelSubsystem ballSubsystem) {
     return new SequentialCommandGroup(
-        // Drive backwards for .25 seconds. The driveArcadeAuto command factory
-        // creates a command which does not end which allows us to control
-        // the timing using the withTimeout decorator
-        driveSubsystem.driveArcade(() -> 0.5, () -> 0).withTimeout(2),
-        // Stop driving. This line uses the regular driveArcade command factory so it
-        // ends immediately after commanding the motors to stop
-        driveSubsystem.driveArcade(() -> 0, () -> 0),
-        // Spin up the launcher for 1 second and then launch balls for 9 seconds, for a
-        // total of 10 seconds
-        ballSubsystem.spinUpCommand().withTimeout(2).andThen(
-        ballSubsystem.launchCommand().withTimeout(11)),
-        // Stop running the launcher
-        ballSubsystem.runOnce(() -> ballSubsystem.stop()));
-  }
+        driveSubsystem.driveArcade(() -> 0.5, () -> 0).withTimeout(0.1),
+        driveSubsystem.driveArcade(() -> 0, () -> 0).withTimeout(0.1),
+        ballSubsystem.spinUpCommand().withTimeout(0.3),
+        ballSubsystem.launchCommand().withTimeout(10));
+}
 }
